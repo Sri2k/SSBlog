7f0737cac53aafd213077c418ab0aa7e6464982e
SriSai_G <gandusrisai@gmail.com>
2023-08-23 18:44:23 +0530

    comments CRUD added components

--- SSBlog/src/app/app.module.ts
+++ SSBlog/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AddCommentComponent } from './components/add-comment/add-comment.component';
+import { CommentComponent } from './components/comment/comment.component';
+import { EditCommentComponent } from './components/edit-comment/edit-comment.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddCommentComponent,
+    CommentComponent,
+    EditCommentComponent
   ],
   imports: [
     BrowserModule,
